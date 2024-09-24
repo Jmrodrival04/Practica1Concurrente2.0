59397bdd5ca4efa06bcb633a3966f4397726e629
pabloalonso308 <113373756+pablonso308@users.noreply.github.com>
2024-09-24 18:39:32 +0200

    modificacion del main para poder generar datos randoms de prueba

--- src/main/java/io/dreamteam/practica1_concurrente/Practica1ConcurrenteApplication.java
+++ src/main/java/io/dreamteam/practica1_concurrente/Practica1ConcurrenteApplication.java
@@ -1,14 +1,12 @@
 package io.dreamteam.practica1_concurrente;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Practica1ConcurrenteApplication {
-
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Practica1ConcurrenteApplication.class, args);
     }
-
 }
